671121e4627e6551f1b86d8832f3eedfbd71d75a
lorena olivares <lorena.olivares.perez16@gmail.com>
2022-05-19 10:36:36 -0300

    requisito 3

---
+++ src/cmponents/Header.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
+
+class Header extends Component {
+  render() {
+    const { email } = this.props;
+    console.log(email);
+    return (
+      <>
+        <h3 data-testid="email-field">{ email }</h3>
+        <span data-testid="total-field">despesas totais: 0</span>
+        <p data-testid="header-currency-field">BRL</p>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: propTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Header from '../cmponents/Header';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <>
+        <Header />
+        <div>TrybeWallet</div>
+      </>
+    );
   }
 }
 
